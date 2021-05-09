47d79188475a07fe5c0dd97683e042d9897229ae
beeunstable6 <beeunstable@protonmail.com>
2021-05-09 23:08:10 +0300

    +++

--- dogs.py
+++ dogs.py
@@ -299,7 +299,7 @@ def train(lr,lm,tpri,rpr,dmc,wd):
 
 
 def hyper():
-    opt = BayesianOptimization(f=train_map,
+    opt = BayesianOptimization(f=train,
             pbounds={'lr':(0.0001, 0.001),
                 'lm':(0.75, 0.95),
                 'tpri':(10, 150),
